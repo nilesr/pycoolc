fd3e043b2a3fbf0afb2be959451ec3e49cb298a0
Niles Rogoff <me@niles.xyz>
2017-03-22 09:47:18 -0400

    Add a ton of comments

--- nfa.py
+++ nfa.py
@@ -1,15 +1,20 @@
-import random
+import random # only used for nfa id, which is only used by pmap
 epsilon_literal = 'ε'
-class field():
+class field(): # a "field" has a list of nodes and a starting node, that's it
     def __init__(self):
         self.nodes = set()
         self.start = False
+# a node is either terminal (accepting) or not, and it has a list of possible moves. 
+# the moves are usually indexed by a character, so my_nfa.moves['a'] will return another nfa
+# ε is special because there can be more than one ε moves, and they don't consume a character of input
+# so my_nfa.moves['ε'] will actually return a set of nodes
 class nfa():
     def __init__(self):
         self.terminal = False
         self.moves = {'ε': set()}
         self.id = random.randrange(1000)
-
+# makes a super simple field with a start node and a terminal node. The start node will move to the end node but only if it's fed the character we were passed.
+# it puts them in a field and sets the start node of the field to the start node
 def build_from_char(c):
     base = nfa()
     end = nfa()
@@ -19,6 +24,15 @@ def build_from_char(c):
     f.start = base
     f.nodes = set([base, end])
     return f
+# this takes a field and allows that field to be repeated zero or more times.
+#                _______________ε_______________
+#               /                               \ 
+#              /                                 v
+# start -> (node) --ε--> (passed field) --ε--> (node) <- that one is terminal
+#              ^             /
+#               \_____ε_____/
+# 
+# 
 def iterate(f):
     new_f = field()
     outer_start = nfa()
@@ -36,6 +50,13 @@ def iterate(f):
     outer_start.moves['ε'] = set([f.start, outer_end])
     new_f.start = outer_start
     return new_f
+# This allows either token in the language
+#                .--ε--> (a) --ε--.
+#               /                  \
+# start -> (node)                 (end) <- that one is terminal
+#               \                  /
+#                `--ε--> (b) --ε--`
+#  
 def either(a, b):
     f = field()
     f.nodes = a.nodes
@@ -54,6 +75,8 @@ def either(a, b):
     f.nodes.add(new_start)
     f.nodes.add(new_end)
     return f
+# returns a field with a then b
+# start -> (a) --ε--> (b) <-- that one has terminal nodes
 def concatenate(a, b):
     f = field()
     f.start = a.start
@@ -65,44 +88,37 @@ def concatenate(a, b):
     for node in b.nodes:
         f.nodes.add(node)
     return f
-
-def union(a, b):
-    r = set()
-    for e in a:
-        r.add(e)
-    for e in b:
-        r.add(e)
-    return r
-
-def add_epsilon_moves(s, ignore=set()):
-    new_s = set(s)
-    if len(ignore) == 0: ignore = new_s
+# takes a set of nodes, returns a set of those nodes and all nodes connected to those nodes by epsilon moves
+# uses an ignore list for its recursive calls so it won't loop infinitely
+def add_epsilon_moves(s, ignore=False):
+    new_s = set(s) # we can't modify the set we're iterating over or the program will explode, so we return a new set instead of modifying s
+    if not ignore:
+        ignore = new_s
     for state in s:
-        # if state in ignore:
-            # continue
-        for possible_move in state.moves['ε']:
+        for possible_move in state.moves['ε']: # for all possible ε moves in all the moves we're expanding over, recurse
             if possible_move in ignore:
                 continue
-            for m in add_epsilon_moves(set([possible_move]), union([possible_move], ignore)):
+            for m in add_epsilon_moves(set([possible_move]),(set([possible_move]) | ignore)):
                 new_s.add(m)
     return new_s
 
+# determines if a compiled regular expression matches a string
 def match(f, inp):
     states = set([f.start])
     idx = 0
     while idx < len(inp):
-        # print(states)
-        states = add_epsilon_moves(states)
-        # print(states)
-        # print()
-        new_states = set()
-        c = inp[idx]
+        states = add_epsilon_moves(states) # expand into epsilon connected states
+        new_states = set() # the states we will be in after we consume the input
+        c = inp[idx] # the character we're taking in right now
         for state in states:
             for key, move in state.moves.items():
                 if key == c:
-                    new_states.add(move)
-        states = new_states;
+                    new_states.add(move) # add this move to new_states if we could get there from one of our old states by consuming that exact character
+        states = new_states; # prepare to do it all over again
         idx += 1
+        if len(states) == 0: # if there are no states, we can't possibly end up at a terminal state so just stop reading
+            return False
+    # now we've consumed all the input. If any of the states we are in are accepting states, it matched, otherwise return false
     for state in states:
         if state.terminal:
             return True
@@ -110,47 +126,48 @@ def match(f, inp):
 
 
 def list_to_field(l):
-    if len(l) == 0:
+    if len(l) == 0: # this base case shouldn't be hit unless you have an empty regex or start your regex with a + or something
+        # all it does is make a field with one terminal node in it
         f = field()
         n = nfa()
         n.terminal = True
         f.start = n
         f.nodes = set([n])
         return f
+    # just maps concatenate over all the things in the list and returns the giant field at the end
     final = l[0]
     for k in l[1:]:
         final = concatenate(final, k)
     return final
-def build_from_regex(regex):
-    to_concat = []
-
-    inparens = False
+def compile(regex):
+    to_concat = [] # empty list of things to concatenate
+    inparens = False # parenthesis parsing stuff
     for i in range(len(regex)):
-        if inparens:
-            if regex[i] == ")":
-                if inparens == 1:
-                    to_concat.append(build_from_regex(subregex)) # FIX
-                    inparens = False
+        if inparens: # if we're in parenthesis
+            if regex[i] == ")": # and we find a close parenthesis
+                if inparens == 1: # and we're now at the same level we started at
+                    to_concat.append(compile(subregex)) # compile the stuff that was in parenthesis and add it to the list
+                    inparens = False # and we're no longer in parenthesis
                 else:
-                    inparens -= 1
-            if regex[i] == "(":
+                    inparens -= 1 # otherwise decrease parens level
+            if regex[i] == "(": # increase parens level if we find a (
                 inparens += 1
-            subregex += regex[i]
-        elif regex[i] == "(":
+            subregex += regex[i] # add the character to the string we will compile when we reach the end of the highest set of parenthesis
+        elif regex[i] == "(": # if we weren't in parens and we find a "(", enter parenthesis
             inparens = 1
             subregex = ""
             continue
-        elif regex[i] == "*":
+        elif regex[i] == "*": # if we find a *, iterate the last thing on the stack, which might have been a subregex (and that's ok)
             to_concat[-1] = iterate(to_concat[-1])
-        elif regex[i] == "+":
-            return either(list_to_field(to_concat), build_from_regex(regex[i+1:])) # kind of a hack and gives + the highest possible operator precedence
-        else:
+        elif regex[i] == "+": # kind of a hack and gives + the highest possible operator precedence
+            return either(list_to_field(to_concat), compile(regex[i+1:]))
+        else: # if we just found a regular character, add it to the stuff to concatenate
             to_concat.append(build_from_char(regex[i]))
     return list_to_field(to_concat)
 
-def addr(node):
+def addr(node): # this used to be a hack that would print the memory address of the node starting with a _ so graphviz didn't split it at the end of the number
     return str(node.id)
-def pmap(f):
+def pmap(f): # prints out the passed field in a way that dot can compile to an svg
     print("digraph test {")
     for node in f.nodes:
         for char, move in node.moves.items():
@@ -167,6 +184,16 @@ def pmap(f):
 
 # DEBUG
 
+# BAD CODE - WILL BREAK EVERYTHING
+# a = build_from_char('a')
+# b = build_from_char('b')
+# full = concatenate(a, b)
+# full2 = concatenate(iterate(a), b)
+# The reason you can't do this is because concatenate is a destructive operation (unsets terminal and adds moves to previously terminal nodes on a)
+# so iterate(a) in the definition of full2 will set all terminal nodes of a to the new terminal node of iterate, but there are no terminal nodes of a so you get a mess. It will actually continue being connected to b as part of full
+# End bad code
+
+# this works
 # full = concatenate(build_from_char('a'), build_from_char('b'))
 # full2 = concatenate(iterate(build_from_char('a')), build_from_char('b'))
 
@@ -179,13 +206,12 @@ def pmap(f):
 
 #print(match(full2, "aab"))
 #pmap(full2)
-#pmap(iterate(a))
 
-#pmap(build_from_regex("ab(c1+2d(e*f)d)*e"))
+#pmap(compile("ab(c1+2d(e*f)d)*e"))
 #pmap(either(build_from_char('a'), build_from_char('b')))
-# x = build_from_regex("(1+0)*1")
+# x = compile("(1+0)*1")
 # #pmap(x)
 # for s in ["101", "111110", "11001", "1", "0"]:
     # print(match(x, s))
-# x = build_from_regex("a+b+c")
+# x = compile("a+b+c")
 # pmap(x)
